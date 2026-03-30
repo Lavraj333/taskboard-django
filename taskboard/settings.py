68692cde4d17b9a504b7adbe653a9a57ab36e7eb
Lavraj333 <lavrajkarna333@gmail.com>
2026-03-30 15:43:00 +0545

    Fixed ALLOWED_HOSTS for deployment

--- taskboard/settings.py
+++ taskboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^4jbwqj!zgh79+4u!2-po^^o=nr(boy(_ken2g!0)93hk-2g51
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
